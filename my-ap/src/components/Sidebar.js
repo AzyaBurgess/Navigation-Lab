c3246f94c8c56e6ef3cf29ed7ca28ae40bac8a1e
AzyaBurgess <Azya.Burgess@cdpipelinedevshop.com>
2022-07-13 16:58:19 +0000

    first commit

---
+++ my-ap/src/components/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../App.css';
+
+function Sidebar(){
+    return(
+        <div className="Sidebar"></div>
+    )
+}
+export default Sidebar
---
+++ my-ap/src/components/SidebarData.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import MailIcon from '@mui/icons-material/Mail';
+
+export const SidebarData={
+    title: "Home",
+    icon: <HomeIcon />,
+    link:"/home",
+}
+{
+   title:"Mail",
+   icon; <MailIcon />,
+   link; "/mail"
+}
+    
+       
+    
